2d66ee28a6634449c8f4d9d2dfcecffcd02fd787
Meri-MG <mgogi13@freeuni.edu.ge>
2023-08-04 11:02:03 +0400

    remove abouts for now

--- db/migrate/20230802052935_add_image_data_to_abouts.rb
+++
@@ -1,5 +0,0 @@
-class AddImageDataToAbouts < ActiveRecord::Migration[7.0]
-  def change
-    add_column :abouts, :image_data, :text
-  end
-end
--- test/models/about_test.rb
+++
@@ -1,9 +0,0 @@
-require "test_helper"
-
-class AboutTest < ActiveSupport::TestCase
-  test "validations" do
-    about = About.create
-    assert_includes about.errors[:title], "can't be blank"
-    assert_includes about.errors[:content], "can't be blank"
-  end
-end
