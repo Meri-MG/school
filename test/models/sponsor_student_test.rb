aae0a0c00acbd9ddd1cc8d8cc557cbb7336b84f4
Meri-MG <mgogi13@freeuni.edu.ge>
2023-08-16 19:24:41 +0400

    add some tests

--- test/models/sponsor_student_test.rb
+++ test/models/sponsor_student_test.rb
@@ -6,5 +6,14 @@ class SponsorStudentTest < ActiveSupport::TestCase
     assert_includes sponsor.errors[:name], "can't be blank"
     assert_includes sponsor.errors[:description], "can't be blank"
     assert_includes sponsor.errors[:age], "can't be blank"
+    assert_includes sponsor.errors[:unique_identifier], "can't be blank"
+  end
+
+  test 'unique identifier' do
+    sponsor = SponsorStudent.new(name: 'Mariam', description: 'good student', age: 12)
+    assert_nil sponsor.unique_identifier
+    byebug
+    sponsor.save
+    assert sponsor.unique_identifier.present?
   end
 end
