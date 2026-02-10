417e12617c2dea5ca84d42653f3261a2139dd939
Meri-MG <mgogi13@freeuni.edu.ge>
2026-02-10 18:49:57 +0400

    normalize tailwind

--- app/javascript/controllers/menu_toggle_controller.js
+++ app/javascript/controllers/menu_toggle_controller.js
@@ -1,13 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="menu-toggle"
 export default class extends Controller {
-  static targets = ["menu", "hamburger"];
+  static targets = ["menu", "bar1", "bar2", "bar3"]
+
+  connect() {
+    console.log("MenuToggleController connected")
+  }
 
   toggleMenu() {
-    this.menuTarget.classList.toggle("change");
-    this.hamburgerTargets.forEach(target => {
-      target.classList.toggle("change");
-    });
+    console.log("Toggling menu")
+    this.menuTarget.classList.toggle("hidden")
+
+    // Animate hamburger to X
+    this.bar1Target.classList.toggle("rotate-45")
+    this.bar1Target.classList.toggle("translate-y-2")
+    this.bar2Target.classList.toggle("opacity-0")
+    this.bar3Target.classList.toggle("-rotate-45")
+    this.bar3Target.classList.toggle("-translate-y-2")
   }
 }
